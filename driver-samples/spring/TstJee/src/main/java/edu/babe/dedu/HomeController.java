f0c3a2a1f0d198a4c54f9d72b4914e742e32b6bc
alexander <alex.komyagin@10gen.com>
2013-05-01 17:09:23 -0400

    Import sprint sample project

---
+++ driver-samples/spring/TstJee/src/main/java/edu/babe/dedu/HomeController.java
@@ -0,0 +1,60 @@
+package edu.babe.dedu;
+
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.Random;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.mongodb.DBCollection;
+import com.mongodb.Mongo;
+
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	@Autowired
+	private MongoTemplate mongoTemplate;
+	
+	public MongoTemplate getMongoTemplate() {
+		return mongoTemplate;
+	}
+
+	public void setMongoTemplate(MongoTemplate mongoTemplate) {
+		this.mongoTemplate = mongoTemplate;
+	}
+
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		MongoOperations mongoOps = mongoTemplate;
+		
+		Maycat cat = new Maycat(new Date().toLocaleString(), new Random().nextInt());
+		mongoOps.insert(cat);
+		
+		List<Maycat> cats = mongoOps.findAll(Maycat.class);
+		model.addAttribute("newObj", cat );
+		model.addAttribute("objects", cats);
+		
+		return "home";
+	}
+	
+}
