388960cc8ddd43451b934206d4aa0a39f93537a0
Gianfranco Palumbo <gianpa@gmail.com>
2013-09-18 10:59:29 +0100

    Updat Java sample simple to v2.10.1 and use MongoClient()

--- driver-samples/java/MongoStandaloneInsertFind.java
+++ driver-samples/java/MongoStandaloneInsertFind.java
@@ -3,14 +3,14 @@ Requirements:
 - Java
 
 How to run this:
-javac -cp .:mongo-2.10.1.jar MongoTest.java && java -cp .:mongo-2.10.1.jar: MongoTest
+javac -cp .:mongo-java-driver-2.11.3.jar MongoStandaloneInsertFind.java && java -cp .:mongo-java-driver-2.11.3.jar: MongoStandaloneInsertFind
 
 Sample output:
 { "_id" : { "$oid" : "517ed98b30041788595947cf"} , "name" : "MongoDB "}
 
 */
 
-import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
 import com.mongodb.DBCollection;
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
@@ -20,7 +20,7 @@ public class MongoStandaloneInsertFind {
     public static void main(String[] args) throws Exception {
 
         // connect to the local database server
-        Mongo m = new Mongo();
+        MongoClient m = new MongoClient();
 
         DB db = m.getDB( "test" );
 
