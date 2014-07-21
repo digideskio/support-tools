86209c424bb9d78e4167ebedc5a1227a5a43be35
Kevin Pulo <kevin.pulo@mongodb.com>
2014-07-21 18:02:35 +1000

    mdiag: remove -w from top command lines
    top -w only works on Debian, not RHEL.
    
    See #62.

--- mdiag/mdiag.sh
+++ mdiag/mdiag.sh
@@ -135,8 +135,8 @@ msection df-k df -k
 msection mount mount
 msection procinfo getfiles /proc/mounts /proc/self/mountinfo /proc/cpuinfo /proc/meminfo /proc/zoneinfo /proc/swaps /proc/modules /proc/vmstat
 msection ps ps -eLFww
-msection top top -b -n 10 -c -w 512
-msection top_threads top -b -n 10 -c -w 512 -H
+msection top top -b -n 10 -c
+msection top_threads top -b -n 10 -c -H
 msection iostat iostat -xtm 5 10
 msection rpcinfo rpcinfo -p
 msection scsidevices getfiles /sys/bus/scsi/devices/*/model
