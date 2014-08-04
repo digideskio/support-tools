34d2ef814b96b77a7eeca35ac9df4c490a59ce6a
Pat Gunn <pgunn@dachte.org>
2014-08-03 21:44:47 -0400

    Greatly fleshing this out
    This does a large enough portion of the work to become interesting.

--- mdiag-windows/mdiag.ps1
+++ mdiag-windows/mdiag.ps1
@@ -12,39 +12,61 @@ $diagfile = $("mdiag-" + $(hostname) + ".txt")
 
 Function section($sname)
 	{
-	# DEFENSIVE: Make sure $thissection is not defined, or return an error
-	$thissection = $sname
-	echo "Gathering section $section"
+	if(_in_section) {throw "Internal error: starting new section [$sname] when already in section [$thissection]";}
+	$script:thissection = $sname
+	echo "Gathering section [$script:thissection]"
 	# TODO: Setup data structures for this section. Maybe output beginning of the JSON paragraph
 	}
 
 Function endsection
 	{
-	echo "Finished with section $thissection. Closing`n"
-	# TODO: undefine $thissection
+	echo "Finished with section [$thissection]. Closing`n"
+	$script:thissection = $Null # Only real way to clear this; Remove-Variable didn't work right
 	# TODO: Clean (flush?) data structures for this section. Maybe output end of JSON paragraph
 	}
 
 Function subsection($ssname)
 	{
-	# DEFENSIVE: Make sure $subsection is not defined, or return an error
-	$subsection = $ssname
-	echo "Gathering subsection $ssname"
+	if(_in_subsection) {throw "Internal error: starting new subsection [$ssname] when already in subsection [$subsection]";}
+	$script:subsection = $ssname
+	echo "Gathering subsection [$script:subsection]"
 	# TODO: Setup data structures for this section. Maybe output beginning of the JSON subparagraph
 	}
 
 Function endsubsection
 	{
-	echo "Finished with subsection $subsection. Closing`n"
-	# TODO: undefine $subsection
+	echo "Finished with subsection [$subsection]. Closing`n"
+	$script:subsection = $Null
 	# TODO: Clean (flush?) data structures for this section. Maybe output end of JSON subparagraph
 	}
 
 Function runcommand
 	{
+	$ok, $txt = _docmd "$args" # Quotes stringify the object
+	if($ok)
+		{
+		echo "Result of [$args]: $txt`n"
+		}
+	else	{
+		echo "Result of [$args]: COMMAND FAILED`n"
+		}
 	}
 Function getfiles
 	{
+	foreach ($filename in $args)
+		{
+		if(_file_exists $filename)
+			{
+			$contents 	= _readfile $filename
+			$stat		= _statfile $filename
+			echo $("File [$filename]: First 2 lines:`n" + $contents[0] + "`n" + $contents[1] + "`n")
+			echo $("File has mode " + $stat[0] + "`n`n")
+			}
+		else
+			{
+			echo "File [$filename]: Does not exist`n"
+			}
+		}
 	}
 ###############
 # Generic internal functions
@@ -53,34 +75,57 @@ Function getfiles
 
 Function _in_section
 	{
+	if($thissection -ne $Null) {return 1;}
+	return 0;
 	}
 Function _in_subsection
 	{
+	if($subsection -ne $Null) {return 1;}
+	return 0;
 	}
 Function _docmd
 	{
-	# TODO: Detect if Invoke-Expression succeeded, parse return code.
+	#Write-Error "Passed arguments [$args]`n"
 	$ret = ""
-	$ret += $(Invoke-Expression($args))
-	return $ret # If we want to change this to a list, prepend with a , to force list context
+	$text = ""
+	$ok = 1;
+	Try 	{
+		#echo "Trying to run command [$args]`n"
+		$text = $(Invoke-Expression "$args -ErrorAction Stop")
+		}
+	Catch	{$ok = 0}
+	if($ok) {$ret = $text}
+	return $ok,$ret
 	}
 Function _file_exists($filename)
 	{
-
+	if(Test-Path $filename) {return 1;}
+	return 0;
 	}
 Function _statfile($filename)
 	{
-
+	# Not sure what the best way to emulate this is.
+	# There is a family of methods that exist as follows:
+	# (Get-ChildItem $filename).methodname
+	# If I can't find a better way to get at this kind of info, I'll build a suitable list that way
+	$fo = Get-ChildItem $filename
+	return $fo.Mode,$fo.IsReadOnly,$fo.CreationTimeUTC,$fo.LastAccessTimeUTC,$fo.LastWriteTimeUTC,$fo.Length
 	}
 Function _readfile($filename)
 	{
-
+	return(Get-Content $filename);
 	}
 ###############
 # JSON functions
 #
 # Please don't call these in the script portion of the code. The API here will never freeze.
 
+# TODO - actually figure out the right abstraction for this.
+
+###############
+# Script-scoped variables
+$script:thissection = $Null
+$script:subsection  = $Null
 
 ###############
 # Script portion
@@ -112,18 +157,6 @@ section stuff3
 endsection
 
 section stuff4
+	getfiles mdiag.ps1
 	getfiles /etc/*release*
 endsection
-
-# Keeping this around as syntax reminder
-#Function msection
-#	$section, $cmd = $args
-#	echo "Gathering $section info... "
-#	$retval = "`n`n=========== start section $section ===========`n"
-#	$retval = $($retval + $(Invoke-Expression($cmd)) + "`n")
-#	$retval = $($retval + "============ end section $section ============`n")
-#	Add-Content $diagfile $retval
-#	echo "done"
-#	}
-#
-#msection Time date
