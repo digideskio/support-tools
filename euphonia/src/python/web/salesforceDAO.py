e63780a9fa55b1cb84cdbbe22960699b7c156df5
Paul Rooney <paul.rooney@mongodb.com>
2014-10-16 12:03:52 -0700

    temporarily disabled sfdc dao

--- euphonia/src/python/web/salesforceDAO.py
+++ euphonia/src/python/web/salesforceDAO.py
@@ -1,5 +1,5 @@
 import requests
-from simple_salesforce import Salesforce
+import pyforce
 from bson import json_util
 
 class salesforceDAO:
@@ -7,15 +7,11 @@ class salesforceDAO:
     def __init__(self):
         self.server = "https://mongodb.my.salesforce.com"
         self.session = "00DA0000000Kz0l!AQgAQD1i1yKkEcQxZDvb4xxrfvnoIn481dgsR1cmvy4Pd5UtURU_nqubueRtdpq8zYD1_..9BLRocjlMO2tuslW6Txf86Cyk"
-        self.connection = Salesforce(instance_url=self.server, session_id=self.session, version="32.0")
+        self.connection = pyforce.PythonClient(serverUrl=self.server)
 
     # QUEUE FUNCTIONS
     def getContacts(self):
-        result = self.connection.Project__c.get('a0OA000000969i0MAA')
+        # result = self.connection.Project__c.get('a0OA000000969i0MAA')
+        result = None
         for item in result:
             print "%s : %s" % (item, result[item])
-
-    def getQueue(self, queueId):
-        getUrl = "%s/queue/%s" % (self.server, queueId)
-        response = self.executeKarakuriCall(getUrl)
-        return response
