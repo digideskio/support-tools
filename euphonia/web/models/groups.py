36b2819cd9f8b9e658ae8753bc7b9c5319e48c10
Paul Rooney <paul.rooney@mongodb.com>
2014-11-03 10:50:45 -0800

    Code comments

--- euphonia/web/models/groups.py
+++ euphonia/web/models/groups.py
@@ -1,21 +1,35 @@
-import pymongo
-
 
 class Groups:
+    """ This class manages the MMS group content.
+    """
 
     def __init__(self, database):
+        """ Initializes Groups class with a database object.
+        :param database: MongoDB client object
+        :return: None
+        """
         self.collection = database.groupsummaries
 
     def get_group_summary(self, gid):
+        """ Retrieves the MMS group summary doc for a given Group ID.
+        :param gid: Group ID
+        :return: Dict containing a single group summary doc
+        """
         query = {"GroupId": gid}
         results = self.collection.find(query)\
-                      .sort("priority", pymongo.DESCENDING)\
                       .limit(1)
         group_summary = next(results, None)
         return group_summary
 
-    def get_failed_tests_summary(self, sort=[("priority", pymongo.DESCENDING)],
+    def get_failed_tests_summary(self, sort=None,
                                  skip=0, limit=10, query=None):
+        """ Retrieves a list of MMS group summary docs
+        :param sort: a (sort_field, sort_order) tuple
+        :param skip: integer defining the number of results to skip
+        :param limit: integer defining the number of results to return
+        :param query: query document to filter the results
+        :return: a dictionary containing the set of result documents
+        """
         fquery = {}
         if query is not None:
             fquery = query
@@ -32,16 +46,30 @@ class Groups:
         return {"groups": groups, "count": group_count}
 
     def ignore_test(self, gid, test):
+        """
+        :param gid:
+        :param test:
+        :return:
+        """
         match = {"GroupId": gid, "failedTests.test": test}
         update = {"$set": {"failedTests.$.ignore": 1}}
         self.collection.update(match, update)
 
     def include_test(self, gid, test):
+        """
+        :param gid:
+        :param test:
+        :return:
+        """
         match = {"GroupId": gid, "failedTests.test": test}
         update = {"$set": {"failedTests.$.ignore": 0}}
         self.collection.update(match, update)
 
     def search(self, query):
+        """
+        :param query:
+        :return:
+        """
         qregex = "^%s" % query
         query = {"GroupName": {"$regex": qregex, "$options": "i"}}
         project = {"GroupName": 1, "GroupId": 1, "_id": 0}
