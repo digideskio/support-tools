fb45ad65fcbed848cd690889800154f6a8763d6d
Bruce Lucas <bruce.lucas@10gen.com>
2014-11-23 12:52:43 -0500

    use SIGTRAP/cont instead of SIGSTOP/SIGCONT
    for some reason SIGSTOP/SIGCONT was causing mongod to hang

--- gdbprof/gdbmon.py
+++ gdbprof/gdbmon.py
@@ -50,17 +50,16 @@ get('^running')
 
 for i in (range(o.count) if o.count else itertools.count()):
     if o.delay: time.sleep(o.delay)
-    if o.dbg: print >>sys.stderr, 'SIGSTOP'
+    if o.dbg: print >>sys.stderr, 'SIGTRAP'
     t0 = time.time()
-    os.kill(int(o.pid), signal.SIGSTOP)
+    os.kill(int(o.pid), signal.SIGTRAP)
     get('*stopped')
     t1 = time.time()
     sys.stdout.write(datetime.datetime.now().strftime('\n=== %FT%T.%f \n'))
     put('thread apply all bt')
     get('^done', True)
     t2 = time.time()
-    if o.dbg: print >>sys.stderr, 'SIGCONT'
-    os.kill(int(o.pid), signal.SIGCONT)
+    if o.dbg: print >>sys.stderr, 'cont'
     put('cont')
     get('^running')
     t3 = time.time()
