13e4d6fc5e478a01ffddcb4589946ccb74cd5d61
Bruce Lucas <bruce.lucas@10gen.com>
2015-01-04 15:28:40 -0500

    fix

--- wt-training/ex3/run.sh
+++ wt-training/ex3/run.sh
@@ -1,25 +1,25 @@
-####
-#### configure for your environment
-####
+#
+# configure for your environment
+#
 tools_dir=~/support-tools
 repro_host=vm-ubuntu
-###
-####
-#### run repro on $repro_host
-####
-###ssh $repro_host bash <<EOF
-###    rm -rf repro
-###    mkdir repro
-###EOF
-###scp repro.* $repro_host:repro
-###scp $tools_dir/timeseries/gdbmon.py $repro_host:repro
-###scp $tools_dir/timeseries/sysmon.py $repro_host:repro
-###ssh -t $repro_host bash <<EOF
-###    cd repro
-###    bash repro.sh
-###EOF
-###
-####
+
+#
+# run repro on $repro_host
+#
+ssh $repro_host bash <<EOF
+    rm -rf repro
+    mkdir repro
+EOF
+scp repro.* $repro_host:repro
+scp $tools_dir/timeseries/gdbmon.py $repro_host:repro
+scp $tools_dir/timeseries/sysmon.py $repro_host:repro
+ssh -t $repro_host bash <<EOF
+    cd repro
+    bash repro.sh
+EOF
+
+#
 # fetch results, visualize them
 #
 scp "$repro_host:repro/*.log" .
